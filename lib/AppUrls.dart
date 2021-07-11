0b3ef4769670a0f27860c5ad80d67dc841e83d94
= <=>
2021-07-11 19:54:25 +0530

    code cleanup

--- lib/AppUrls.dart
+++ lib/AppUrls.dart
@@ -1,5 +1,3 @@
 class AppUrls {
-  static var mainUrl = 'https://cricapi.com/api';
-  static var getMatches = '$mainUrl/matches';
   static var cricketNewsUrl = 'https://newsapi.org/v2/everything?q=cricket&';
 }
